c0e3365eb4a8d5a2f56f0b0af4903dbef81f7741
Pedro Santana <97036217+pedrohsantanaa@users.noreply.github.com>
2025-07-03 21:03:10 -0300

    V6

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Tarefas
 
 # Create your views here.
 def listaTarefa(request):
-    return render(request, 'tarefas/list.html')
+    tarefas_list = Tarefas.objects.all().order_by('-created_at')
+    return render(request, 'tarefas/list.html', {'tarefas':tarefas_list})
