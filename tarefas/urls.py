9bc00465e677cfd8fc1e10f80bb27a30fd594af7
Pedro Santana <97036217+pedrohsantanaa@users.noreply.github.com>
2025-07-10 19:52:58 -0300

    V9

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.listaTarefa, name='lista-tarefa'),
-    path('novaTarefa/', views.novaTarefa, name='nova-tarefa')
+    path('novaTarefa/', views.novaTarefa, name='nova-tarefa'),
+    path('tarefa/<int:id>', views.tarefaView, name='tarefa-view'),
+    path('edit/<int:id>', views.editTarefa, name='edit-tarefa'),
+    path('delete/<int:id>', views.deleteTarefa, name='delete-tarefa'),
  
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarefas
 from .forms import TarefaForm
 
@@ -17,3 +17,27 @@ def novaTarefa(request):
     else:
         form = TarefaForm()
         return render(request, 'tarefas/addTarefa.html', {'form':form})
+    
+def tarefaView(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    return render(request, 'tarefas/tarefa.html', {'tarefa':tarefa})
+
+def editTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    form = TarefaForm(instance=tarefa)
+
+    if(request.method == 'POST'):
+        form = TarefaForm(request.POST, instance=tarefa)
+
+        if(form.is_valid()):
+            tarefa.save()
+            return redirect('/')
+        else:
+            return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+    
+    else:
+            return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+def deleteTarefa(request, id):
+     tarefa = get_object_or_404(Tarefas, pk=id)
+     tarefa.delete()
+     return redirect('/')
